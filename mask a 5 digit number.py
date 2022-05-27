4912edda45936e19768f4f269df7031f720a41f2
intellectoglobal <intellectoglobal@gmail.com>
2022-05-26 22:58:41 -0700

    test

---
+++ mask a 5 digit number.py
@@ -0,0 +1,16 @@
+
+def maska5digitnumber():
+    str = input("Enter the mobile number: ")
+    masked = len(str) - 5
+    slimstr = str[masked:]
+    print("Your masked card number is: ", end="")
+    print("*" * masked + slimstr)
+    
+maska5digitnumber()
+def maska5digitnumber():
+    str = input("Enter the mobile number: ")
+    masked = len(str) - 5
+    slimstr = str[masked:]
+    print("Your masked card number is: ", end="")
+    print("*" * masked + slimstr)
+maska5digitnumber()
